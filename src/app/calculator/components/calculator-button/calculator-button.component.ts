643c66a2cf87fa9c008776166e1d6ec8a32ed042
Juan Carlos <gurquiza8@gmail.com>
2024-09-16 21:58:45 -0600

    feature: InputSignal y HostBindigs

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,16 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
   standalone: true,
   imports: [],
   templateUrl: './calculator-button.component.html',
-  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: './calculator-button.component.css',
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
   }
 })
 export class CalculatorButtonComponent {
+
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
+  @HostBinding('class.is-command')
+  get commandStyle() {
+    return this.isCommand()
+  }
 
 }
