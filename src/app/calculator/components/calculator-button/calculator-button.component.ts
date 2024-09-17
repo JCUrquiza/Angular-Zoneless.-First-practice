c805ff03507f68f746fc3bdf72a5c0f56120f090
Juan Carlos <gurquiza8@gmail.com>
2024-09-16 22:19:07 -0600

    feature: viewEncapsulation concept

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -9,7 +9,8 @@ import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit } from '
   styles: './calculator-button.component.css',
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
-  }
+  },
+  // encapsulation: ViewEncapsulation.None
 })
 export class CalculatorButtonComponent {
 
@@ -18,9 +19,19 @@ export class CalculatorButtonComponent {
       typeof value === 'string' ? value === '' : value
   });
 
-  @HostBinding('class.is-command')
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value
+  })
+
+  // @HostBinding('class.is-command')
+  // get commandStyle() {
+  //   return this.isCommand()
+  // }
+
+  @HostBinding('class.w-2/4')
   get commandStyle() {
-    return this.isCommand()
+    return this.isDoubleSize()
   }
 
 }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -6,8 +6,12 @@ import { CalculatorButtonComponent } from "../calculator-button/calculator-butto
   standalone: true,
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  // styles: `
+  //   .iscommand {
+  //     @apply bg-indigo-700 bg-opacity-20;
+  //   }
+  // `,
 })
 export class CalculatorComponent {
 
