f9ff9d1799ef99f3661c4a2067000924cb96e134
Juan Carlos <gurquiza8@gmail.com>
2024-09-25 15:51:20 -0600

    feature: finish proofs in app.component.html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,7 +26,33 @@ describe('AppComponent', () => {
   });
 
   it('should render router-outlet', () => {
-    expect(compiled.querySelector).not.toBeNull();
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
+  });
+
+  it('should render router-outlet wrapped with css classes', () => {
+    const divElement = compiled.querySelector('div');
+
+    const mustHaveClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+
+    expect(divElement).not.toBeNull();
+
+    // divElement?.classList.forEach((className) => {
+    //   expect(mustHaveClasses).toContain(className);
+    // });
+
+    const divClasses = divElement?.classList.value.split(' ');
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className)
+    });
+  });
+
+  it("should contain the 'buy me a beer' link", () => {
+    const aElement = compiled.querySelector('a');
+
+    const aAtributeHref = aElement?.getAttribute('href');
+
+    expect(aElement?.title).toBe('Buy me a beer');
+    expect(aAtributeHref ).toBe('https://www.buymeacoffee.com/scottwindon');
   });
 
 });
